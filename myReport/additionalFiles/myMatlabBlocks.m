3ad8ecd186d83ac9b313b1528147d96e75100cbd
Eniz Mušeljić <eniz.m@outlook.com>
2018-07-08 17:18:53 +0200

    fixes and updated docu

--- myReport/additionalFiles/myMatlabBlocks.m
+++ myReport/additionalFiles/myMatlabBlocks.m
@@ -8,19 +8,87 @@ report.addTabular(arr);
 row_labels = 'row1,row2';
 col_labels = 'col1,col2,col3';
 
+report.set('dataFormat','%i');
 report.set('tableRowLabels', row_labels);
 report.set('tableColLabels', col_labels);
 report.set('tableColumnAlignment','lccc');
-%report.set('dataFormat','%.1f')
+
+report.addTabular(arr);
+
+%% TABFUN21
+
+row_labels = 'row1,row2';
+col_labels = 'col1,col2,col3';
+
+report.set('tableRowLabels', row_labels);
+report.set('tableColLabels', col_labels);
+report.set('tableColumnAlignment','lccc');
 
 report.addTabular(arr);
 
 %% TABFUN3
 
-arr = randn(4,4)*5;
-
-%report.set('dataFormat','%0.2f %0.3f %0.1f %0.2f');
-report.set('tableColumnAlignment','l|r|r');
+report.set('dataFormat','%0.1f,%0.3f,%0.1f,%0.2f');
+report.set('tableColLabels','col1,col2,col3,col4');
+report.set('tableRowLabels','row1,row2,row3,row4');
 report.set('booktabs',1);
 
+arr = randn(4,4)*5;
+arr(3) = nan;
 report.addTabular(arr);
+
+%% TABFUN31
+
+
+report.set('tableColLabels','col1,col2,col3,col4');
+report.set('tableRowLabels','row1,row2,row3,row4');
+report.set('booktabs',1);
+
+arr = randn(4,4)*5;
+arr(3) = nan;
+report.addTabular(arr);
+
+
+%% TABFUN4
+
+report.set('tableRowLabels','row1, ,row3,row4');
+report.set('tableColumnAlignment','c|cccc');
+report.addTabular(arr);
+
+%% TABFUN5
+
+report.setDefault('table');
+report.set('tableBorders', 0);
+report.addTabular(arr);
+
+%% TABFUN6
+
+LastName = {'Smith';'Johnson';'Williams';'Jones';'Brown'};
+Age = [38;43;38;40;49];
+Height = [71;69;64;67;64];
+Weight = [176;163;131;133;119];
+T = table(Age,Height,Weight,'RowNames',LastName);
+
+report.addTabular(T);
+
+%% TABFUN7
+
+report.set('transposeTable',1);
+report.addTable(T, 'My caption');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
